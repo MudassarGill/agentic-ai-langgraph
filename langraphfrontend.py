135bf1dc88dc65ccae1cebd94d9c23c331a2dcd1
MudassarGill <mudassarhussain6533@gmail.com>
2026-04-27 21:11:36 -0700

    front end added

--- langraphfrontend.py
+++ langraphfrontend.py
@@ -1,5 +1,35 @@
+#Here we use streamming and also with slide bar where our old and new converstion is stored just like chatgpt for 
+#we have a buttion with new chat and also we have my coversion in the left bar where we can delete the converstion or rename it
 import streamlit as st
-from langraphbackend import Chatbot
+from langgraphbackend import app
+from typing import TypedDict,Annotated
+from langchain_core.messages import HumanMessage,AIMessage,add_messages
 
-with st.chat_message("user"):
-    st.text("Hi")
+class State(TypedDict):
+    messages:Annotated[list,add_messages]
+
+st.title("Chatbot")
+
+if "messages" not in st.session_state:
+    st.session_state.messages=[]
+
+for message in st.session_state.messages:
+    with st.chat_message(message.type):
+        st.markdown(message.content)
+
+if prompt:=st.chat_input("You are a helpful assistant"):
+    st.session_state.messages.append(HumanMessage(content=prompt))
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    
+    with st.chat_message("assistant"):
+        message_placeholder=st.empty()
+        full_response=""
+        for chunk in app.stream({"messages":st.session_state.messages}):
+            if "messages" in chunk:
+                for msg in chunk["messages"]:
+                    if msg.type=="ai" and msg.content:
+                        full_response+=msg.content
+            message_placeholder.markdown(full_response+"▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append(AIMessage(content=full_response))
