02ccaba76bc48b8c25cb18d3f96b2e7cc7a9c01d
MudassarGill <mudassarhussain6533@gmail.com>
2026-04-27 21:28:05 -0700

    updatedd

--- langraphfrontend.py
+++ langraphfrontend.py
@@ -1,9 +1,13 @@
 #Here we use streamming and also with slide bar where our old and new converstion is stored just like chatgpt for 
 #we have a buttion with new chat and also we have my coversion in the left bar where we can delete the converstion or rename it
+# we add also thread id with im memory sever which save our conversation and we can access it later
 import streamlit as st
 from langgraphbackend import app
 from typing import TypedDict,Annotated
 from langchain_core.messages import HumanMessage,AIMessage,add_messages
+from langgraph.checkpoint.memory import InMemorySaver
+
+checkpointer = InMemorySaver()
 
 class State(TypedDict):
     messages:Annotated[list,add_messages]
